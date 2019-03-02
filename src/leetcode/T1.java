f3a961ba9b75cabe2bcd47d8350d6dcd91e8e2ee
yuuteng <yuuteng@foxmail.com>
2019-03-02 23:53:48 +0100

    T1

---
+++ src/leetcode/T1.java
@@ -0,0 +1,30 @@
+package leetcode;
+/**1. Two Sum (easy)
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ *
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * Example:
+ *
+ * Given nums = [2, 7, 11, 15], target = 9,
+ *
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ * */
+public class T1 {
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+    public static void main(String[] args) {
+        for (int i : twoSum(new int[]{2, 7, 11, 15}, 9)) {
+            System.out.println(i);
+        }
+    }
+}
